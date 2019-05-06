b13cb3ec267b6c475000a42ad733050a70d9ec13
Thomas Coupechoux thomas.coupechoux@univ-grenoble-alpes.fr <coupecht@f-dahu.u-ga.fr>
2019-05-06 16:02:21 +0200

    test sur luke

--- ciment/hello/default.nix
+++ ciment/hello/default.nix
@@ -1,4 +1,9 @@
 { stdenv, fetchurl }:
+let
+	icc=/nix/store/3lf5gc31c8325x7j3gib95b24kfxr1vy-intel-compilers-2017/compilers_and_libraries_2017.4.196/linux/bin/intel64/icc
+	icpc=/nix/store/3lf5gc31c8325x7j3gib95b24kfxr1vy-intel-compilers-2017/compilers_and_libraries_2017.4.196/linux/bin/intel64/icpc
+	ifort=/nix/store/3lf5gc31c8325x7j3gib95b24kfxr1vy-intel-compilers-2017/compilers_and_libraries_2017.4.196/linux/bin/intel64/ifort
+	intel-library=/nix/store/3lf5gc31c8325x7j3gib95b24kfxr1vy-intel-compilers-2017/
 
 stdenv.mkDerivation rec {
   name = "hello-2.10";
@@ -7,7 +12,11 @@ stdenv.mkDerivation rec {
     url = "mirror://gnu/hello/${name}.tar.gz";
     sha256 = "0ssi1wpaf7plaswqqjwigppsg5fyh99vdlb9kzl7c9lng89ndq1i";
   };
-
+	configureFlagsArray=["-I /usr/include/x86_64-linux-gnu/"];
+	configureFlags=[
+	"CC=${icc}"
+	"CXX=${icpc}"
+	];
   doCheck = true;
 
   meta = {
