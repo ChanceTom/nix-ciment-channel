4f45661057ec0b0d5569dd24e210a328319fa3af
Thomas Coupechoux thomas.coupechoux@univ-grenoble-alpes.fr <coupecht@f-dahu.u-ga.fr>
2019-05-13 15:23:37 +0200

    test de hello

--- ciment/hello/default.nix
+++ ciment/hello/default.nix
@@ -1,10 +1,9 @@
 { stdenv, fetchurl }:
 let
-	icc=/nix/store/3lf5gc31c8325x7j3gib95b24kfxr1vy-intel-compilers-2017/compilers_and_libraries_2017.4.196/linux/bin/intel64/icc
-	icpc=/nix/store/3lf5gc31c8325x7j3gib95b24kfxr1vy-intel-compilers-2017/compilers_and_libraries_2017.4.196/linux/bin/intel64/icpc
-	ifort=/nix/store/3lf5gc31c8325x7j3gib95b24kfxr1vy-intel-compilers-2017/compilers_and_libraries_2017.4.196/linux/bin/intel64/ifort
-	intel-library=/nix/store/3lf5gc31c8325x7j3gib95b24kfxr1vy-intel-compilers-2017/
-
+	icc=/nix/store/3lf5gc31c8325x7j3gib95b24kfxr1vy-intel-compilers-2017/compilers_and_libraries_2017.4.196/linux/bin/intel64/icc;
+	icpc=/nix/store/3lf5gc31c8325x7j3gib95b24kfxr1vy-intel-compilers-2017/compilers_and_libraries_2017.4.196/linux/bin/intel64/icpc;
+	ifort=/nix/store/3lf5gc31c8325x7j3gib95b24kfxr1vy-intel-compilers-2017/compilers_and_libraries_2017.4.196/linux/bin/intel64/ifort;
+in
 stdenv.mkDerivation rec {
   name = "hello-2.10";
 
@@ -12,12 +11,12 @@ stdenv.mkDerivation rec {
     url = "mirror://gnu/hello/${name}.tar.gz";
     sha256 = "0ssi1wpaf7plaswqqjwigppsg5fyh99vdlb9kzl7c9lng89ndq1i";
   };
-	configureFlagsArray=["-I /usr/include/x86_64-linux-gnu/"];
-	configureFlags=[
-	"CC=${icc}"
-	"CXX=${icpc}"
-	];
+	configureFlagsArray=["LDFLAGS=-I /usr/include/x86_64-linux-gnu/"];
+	configureFlags=["CC=${icc}"
+        "CXX=${icpc}"];
+
   doCheck = true;
+
 
   meta = {
     description = "A program that produces a familiar, friendly greeting";
